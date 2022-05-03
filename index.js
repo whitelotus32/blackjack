f5ad7c0614015beacc2d619c4f0f179d182403ab
whitelotus32 <87887692+whitelotus32@users.noreply.github.com>
2022-05-03 11:41:33 -0700

    Updated game logic and added message var

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ let firstCard = 6
 let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
+    message = "Do you want to draw a new card?"
 } else if (sum === 21) {
-    console.log("Woohoo! You've got Blackjack!")
+    message = "Woohoo! You've got Blackjack!"
     hasBlackJack = true
 } else {
-    console.log("You're out of the game!")
+    message = "You're out of the game!"
+    isAlive = false
 }
 
