c26a6ee5052dc6e734e8fa46231a7188d333d9a9
whitelotus32 <87887692+whitelotus32@users.noreply.github.com>
2022-05-04 11:25:08 -0700

    Added New Card feature

--- index.js
+++ index.js
@@ -26,3 +26,8 @@ function startGame() {
     }
     messageEl.textContent = message
 }
+
+// Created a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
