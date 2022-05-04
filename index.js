3eb38d4715c176a4569315ce31561996aeeb2078
whitelotus32 <87887692+whitelotus32@users.noreply.github.com>
2022-05-04 06:03:07 -0700

    Created a start button

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Woohoo! You've got Blackjack!"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game!"
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
 }
-
