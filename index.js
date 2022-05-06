d3063371d2d6c1e8fb5b8d1c0c1ccf3f7c04680d
whitelotus32 <87887692+whitelotus32@users.noreply.github.com>
2022-05-06 05:56:48 -0700

    Assigned values to the startGame function

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-let firstCard = 6
-let secondCard = 9
-// Created an array - cards - that contains firstCard and secondCard
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -27,6 +24,11 @@ function getRandomCard() {
 
 // Created a new function called startGame() that calls renderGame()
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
